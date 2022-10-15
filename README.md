b7a75329cdcffe7de69a46220006a7c3df6e1237
LogonukIgor <100472895+LogonukIgor@users.noreply.github.com>
2022-10-15 14:45:51 +0300

    Hiding mail data & Editing README.md

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# carsrent
+# CarsRentApp
+___
+### To work correctly, follow these steps:
+
+#### 1) create the ***mail.properties*** file in the ***resources*** package (api module);
+
+#### 2) fill it out as follows:  
+- HOST=smtp.gmail.com   
+- PORT=port  
+- USER_NAME=gmail  
+- PASSWORD=password   
+- PROTOCOL=smtp/smtps
--- api/src/main/java/by/logonuk/configuration/MailSenderConfig.java
+++ api/src/main/java/by/logonuk/configuration/MailSenderConfig.java
@@ -1,36 +1,49 @@
 package by.logonuk.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
 @Configuration
+@PropertySource("classpath:mail.properties")
 public class MailSenderConfig {
 
+    @Value("${HOST}")
+    private String host;
+
+    @Value("${PORT}")
+    private Integer port;
+
+    @Value("${USER_NAME}")
+    private String userName;
+
+    @Value("${PASSWORD}")
+    private String password;
+
+    @Value("${PROTOCOL}")
+    private String protocol;
     @Bean
     public JavaMailSender getMailSender() {
 
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
-//        mailSender.setHost("smtp.yandex.ru");
-//        mailSender.setPort(465);
-//        mailSender.setUsername("logonukapi@yandex.by");
-//        mailSender.setPassword("yzhmtwzcnyczvntc");
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-        mailSender.setUsername("logonukapi@gmail.com");
-        mailSender.setPassword("lgkfcxxhyzgbcbbd");
-        mailSender.setProtocol("smtp");
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(userName);
+        mailSender.setPassword(password);
+        mailSender.setProtocol(protocol);
 
         Properties properties = mailSender.getJavaMailProperties();
 
         properties.setProperty("mail.transport.protocol", "true");
         properties.setProperty("mail.smtp.auth", "true");
         properties.setProperty("mail.smtp.starttls.enable", "true");
-//        properties.setProperty("mail.debug", "true");
+        properties.setProperty("mail.debug", "true");
 
         return mailSender;
     }
